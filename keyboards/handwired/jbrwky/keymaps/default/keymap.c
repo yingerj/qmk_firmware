67b79128ce449278c9bc4ca053f9e645564de0a4
Jack Yinger <jack.yinger@gmail.com>
2020-12-09 19:14:58 -0800

    Committing before I change this layout beyond recognition hand have to hard reset...

--- keyboards/handwired/jbrwky/keymaps/default/keymap.c
+++ keyboards/handwired/jbrwky/keymaps/default/keymap.c
@@ -19,20 +19,36 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include QMK_KEYBOARD_H
 
+#define ____ KC_TRNS
+
+#define KC_SHFT KC_RSHIFT
+
+#define JC_RARW KC_RIGHT
+#define JC_LARW KC_LEFT
+#define JC_UARW KC_UP
+#define JC_DARW KC_DOWN
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT(
-  //,--------------------------------------------.            ,--------------------------------------------.
-         KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                 KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
-  //|--------+--------+--------+--------+--------|            |--------+--------+--------+--------+--------|
-         KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                 KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN,
-  //|--------+--------+--------+--------+--------|            |--------+--------+--------+--------+--------|
-         KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                 KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,
-  //|--------+--------+--------+--------+--------+            +--------+--------+--------+--------+--------|
-                                    KC_0,    KC_1,               KC_2  ,   KC_3 ,
-                              //`----------------'            `----------------'
-                                    KC_4,    KC_5,               KC_6  ,   RESET
-                              //`----------------'            `----------------'
+        KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,              KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
 
+        KC_A,    KC_S,    KC_D,    KC_F,    KC_G,              KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,
+
+        KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,              KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,
+
+                                   MO(1),   KC_ESC,            KC_BSPC, KC_SPC,
+
+                                   KC_LCTL, KC_LALT,           KC_SHFT, KC_TAB
+  ),
+  [1] =LAYOUT(
+        KC_1,    KC_2,    KC_3,    KC_4,    KC_5,              KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
+
+        KC_GRAVE,____,    ____,    ____,    ____,              RESET,   ____,    KC_LBRC, KC_RBRC, KC_BSLASH,
+
+        ____,    ____,    ____,    ____,    ____,              ____,    ____,    ____,    KC_QUOTE,____,
+
+                                   ____,    ____,              JC_UARW, JC_RARW,
+
+                                   ____,    ____,              JC_LARW, JC_DARW
   )
 };
-
